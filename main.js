42f1e72565b7736cc4bd2f0c4db4836f8db39bc1
attilioborea <attilio783@gmail.com>
2022-09-14 19:21:25 +0200

    fix

--- main.js
+++ main.js
@@ -78,25 +78,38 @@ scene.add(cube);*/
 
 
 let loader = new THREE.GLTFLoader();
-var obj;
+var pg;
+var ObjectsPg = new Array();
 loader.load("scene.gltf", function(gltf){
-  obj = gltf.scene;
-  obj.position.y -= 0.3;
-  //obj.scale.x = 0.2;
-  //obj.scale.y = 0.2;
-  //obj.scale.z = 0.2;
+  pg = gltf.scene;
+  pg.position.y -= 0.34;
+  //pg.scale.x = 0.2;
+  //pg.scale.y = 0.2;
+  //pg.scale.z = 0.2;
   scene.add(gltf.scene);
+
+
+
+
+
+
+
+
+
+
+
 })
 
+/*
 let loader1 = new THREE.GLTFLoader();
 var boot;
 loader1.load("snowboots/scene.gltf", function(gltf){
   boot = gltf.scene;
   boot.position.y -= 0.25;
   boot.position.z += 0.3;
-  boot.scale.x = 10;
-  boot.scale.y = 10;
-  boot.scale.z = 10;
+  boot.scale.x = 7;
+  boot.scale.y = 7;
+  boot.scale.z = 7;
   scene.add(gltf.scene);
 })
 
@@ -106,14 +119,14 @@ loader2.load("snowboots/scene.gltf", function(gltf){
   boot2 = gltf.scene;
   boot2.position.y -= 0.25;
   boot2.position.z -= 0.3;
-  boot2.scale.x = 10;
-  boot2.scale.y = 10;
-  boot2.scale.z = 10;
+  boot2.scale.x = 7;
+  boot2.scale.y = 7;
+  boot2.scale.z = 7;
   scene.add(gltf.scene);
 })
 
 
-
+*/
   
 
 
@@ -179,6 +192,8 @@ loaderHouseDoor.load(
   }
 );
 
+
+
 var flagHouseWindow;
 let loaderHouseWindow = new THREE.GLTFLoader();
 var HouseWindow;
@@ -201,6 +216,9 @@ loaderHouseWindow.load(
     console.error(error);
   }
 );
+
+
+//Cart
 
 var flagHouseCart;
 let loaderHouseCart = new THREE.GLTFLoader();
@@ -226,24 +244,231 @@ loaderHouseCart.load(
   }
 );
 
-// const directionalLight2 = new THREE.PointLight(0xffc966, 9, 5);
-// directionalLight2.position.x = -19;
-// directionalLight2.position.y = +5.5;
-// directionalLight2.position.z = -20;
-// if (flagHouseDoor) {
-//   directionalLight2.target = HouseDoor;
-// }
-// scene.add(directionalLight2);
+//Light house
 
-// const directionalLight3 = new THREE.PointLight(0xffc966, 15, 6);
-// directionalLight3.position.x = -18;
-// directionalLight3.position.y += 1;
-// directionalLight3.position.z = -20;
+const directionalLight = new THREE.PointLight(0xffc966, 15, 6);
+directionalLight.position.x = -20;
+directionalLight.position.y += 6.5;
+directionalLight.position.z = -29;
 
-// scene.add(directionalLight3);
+scene.add(directionalLight);
 
 
+//Trees
 
+var flagSnowTrees1;
+let loaderSnowTrees1 = new THREE.GLTFLoader();
+
+var indexSnowTrees = 0;
+
+var SnowTrees1;
+loaderSnowTrees1.load(
+  "snowtree/scene.gltf",
+  function (gltf) {
+    SnowTrees1 = gltf.scene;
+
+     SnowTrees1.scale.x = 0.008;
+   SnowTrees1.scale.y = 0.008;
+    SnowTrees1.scale.z = 0.007;
+  SnowTrees1.position.y -= 0.5;
+
+     SnowTrees1.position.z -= 15;
+     SnowTrees1.position.x -= 40;
+
+   
+
+    scene.add(SnowTrees1);
+
+    flagSnowTrees1 = true;
+  },
+  undefined,
+  function (error) {
+    console.error(error);
+  }
+);
+
+var flagSnowTrees2;
+let loaderSnowTrees2 = new THREE.GLTFLoader();
+
+var indexSnowTrees = 0;
+
+var SnowTrees2;
+loaderSnowTrees2.load(
+  "snowtree/scene.gltf",
+  function (gltf) {
+    SnowTrees2 = gltf.scene;
+
+     SnowTrees2.scale.x = 0.008;
+   SnowTrees2.scale.y = 0.008;
+    SnowTrees2.scale.z = 0.007;
+  SnowTrees2.position.y -= 0.5;
+
+     SnowTrees2.position.z += 14;
+     SnowTrees2.position.x -= 25;
+
+   
+
+    scene.add(SnowTrees2);
+
+    flagSnowTrees2 = true;
+  },
+  undefined,
+  function (error) {
+    console.error(error);
+  }
+);
+
+var flagSnowTrees3;
+let loaderSnowTrees3 = new THREE.GLTFLoader();
+
+var indexSnowTrees = 0;
+
+var SnowTrees3;
+loaderSnowTrees3.load(
+  "snowtree/scene.gltf",
+  function (gltf) {
+    SnowTrees3 = gltf.scene;
+
+    SnowTrees3.scale.x = 0.008;
+    SnowTrees3.scale.y = 0.008;
+    SnowTrees3.scale.z = 0.007;
+    SnowTrees3.position.y -= 0.5;
+
+    SnowTrees3.position.z += 2;
+    SnowTrees3.position.x += 15;
+
+   
+
+    scene.add(SnowTrees3);
+
+    flagSnowTrees3= true;
+  },
+  undefined,
+  function (error) {
+    console.error(error);
+  }
+);
+
+
+
+/*
+var flagSnowMan;
+let loaderSnowMan = new THREE.GLTFLoader();
+var snowMan ;
+
+loaderSnowMan.load(
+  "snowman/scene.gltf",
+  function(gltf) {
+    snowMan = gltf.scene;
+    snowMan.rotation.y -= Math.PI / 1.3;
+    snowMan.position.x += 5;
+    snowMan.position.z -= 10;
+    snowMan.scale.x = 5;
+    snowMan.scale.y = 5;
+    snowMan.scale.z = 5;
+
+    scene.add(snowMan);
+    flagSnowMan = true;
+  });
+
+  var flagSnowMountain;
+  let loaderSnowMountain = new THREE.GLTFLoader();
+  var SnowMountain ;
+  
+  loaderSnowMountain.load(
+    "mountain/scene.gltf",
+    function(gltf) {
+      SnowMountain = gltf.scene;
+      SnowMountain.rotation.y -= Math.PI / 2;
+      SnowMountain.position.x += 55;
+      SnowMountain.position.z -= 95;
+      SnowMountain.position.y -= 4;
+      SnowMountain.scale.x = 0.7;
+      SnowMountain.scale.y = 0.7;
+      SnowMountain.scale.z = 0.7;
+  
+      scene.add(SnowMountain);
+      flagSnowMountain = true;
+    });
+*/
+var flagScore;
+let loaderScore = new THREE.GLTFLoader();
+var Score;
+
+loaderScore.load(
+  "score/scene.gltf",
+  function(gltf){
+    Score = gltf.scene;
+    Score.scale.x = 10;
+    Score.scale.y = 10;
+    Score.scale.z = 10;
+
+    Score.position.x -= 8;
+    Score.position.y -= 0.75;
+    Score.position.z -= 22;
+
+    scene.add(Score);
+    flagScore = true;
+    const geometryBoardPaper = new THREE.BoxGeometry(3.2, 1.8, 0.1);
+    const textureBoardPaper = new THREE.TextureLoader().load(
+      "textures/BoardPaperTexture.jpg"
+    );
+    const materialBoardPaper = new THREE.MeshBasicMaterial({
+      map: textureBoardPaper
+});
+const BoardPaper = new THREE.Mesh(geometryBoardPaper, materialBoardPaper);
+    BoardPaper.position.y += 0.3;
+    BoardPaper.position.x += 0.01;
+    BoardPaper.position.z += 0.03;
+    BoardPaper.rotation.y -= Math.PI;
+    BoardPaper.scale.x = 0.12;
+    BoardPaper.scale.y = 0.12;
+    BoardPaper.scale.z = 0.12;
+    ObjectsPg.push(BoardPaper);
+    Score.add(BoardPaper);
+
+    const loaderGeometry = new THREE.FontLoader();
+
+    loaderGeometry.load(
+      "fonts/helvetiker_regular.typeface.json",
+      function (font) {
+        const geometryPaperText = new THREE.TextGeometry(
+          "TOTAL SNOWBALLS \n COLLECTED: 0",
+          {
+            font: font,
+            size: 5,
+            height: 1,
+            curveSegments: 12,
+            bevelEnabled: true,
+            bevelThickness: 1,
+            bevelSize: 0.2,
+            bevelOffset: 0,
+            bevelSegments: 3,
+          }
+        );
+        const texturePaperText = new THREE.TextureLoader().load(
+          "textures/BoardPaperTexture.jpg"
+        );
+        const materialPaperText = new THREE.MeshBasicMaterial({
+          color: 0xffa500,
+        });
+        const PaperText = new THREE.Mesh(geometryPaperText, materialPaperText);
+        ObjectsPg.push(PaperText);
+        PaperText.scale.x = 0.05;
+        PaperText.scale.y = 0.05;
+        PaperText.scale.z = 0.05;
+        PaperText.position.y += 2.5;
+        PaperText.position.z -= 21.6;
+        PaperText.position.x -= 9.5;
+        scene.add(PaperText);
+      }
+    );
+  },
+  undefined,
+  function (error) {
+    console.error(error);
+  }
+);
 
 const animate = function () {
   requestAnimationFrame(animate);
